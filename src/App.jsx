eb18472051a789e5bce65f99588992dc5339f5f8
Long Hoang <phl19.dev@gmail.com>
2023-11-16 00:14:00 +0700

    setup projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const Container = styled.div`
+  padding: 20px;
+  background-color: orangered;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyles />
+      <Container>
+        <Heading>The Wild Oasis</Heading>
+        <Heading as="h2">Input</Heading>
+        <Input placeholder="he" type="number" />
+        <Heading as="h3">Button</Heading>
+        <Button>check</Button>
+        <Button onClick={() => alert("check")}>check</Button>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/ui/Heading.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  font-size: 20px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+export default Heading;
